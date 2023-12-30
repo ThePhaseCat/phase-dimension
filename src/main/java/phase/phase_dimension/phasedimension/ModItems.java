47ea4037a59ec46de72f5464c42e978707b8b0c2
ThePhaseCat <alexmgamer10@gmail.com>
2023-12-29 18:33:38 -0700

    test stuff

---
+++ src/main/java/phase/phase_dimension/phasedimension/ModItems.java
@@ -0,0 +1,31 @@
+package phase.phase_dimension.phasedimension;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item POOP = register(
+            new Item(new FabricItemSettings()),
+            "poop"
+    );
+
+    public static void initialize() {}
+
+    // We can use generics to make it so we dont need to
+    // cast to an item when using this method.
+    public static <T extends Item> T register(T item, String ID) {
+        // Create the identifier for the item.
+        Identifier itemID = new Identifier("mod_id", ID);
+
+        // Register the item.
+        T registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return the registered item!
+        return registeredItem;
+    }
+
+}
--- src/main/java/phase/phase_dimension/phasedimension/PhaseDimension.java
+++ src/main/java/phase/phase_dimension/phasedimension/PhaseDimension.java
@@ -1,6 +1,8 @@
 package phase.phase_dimension.phasedimension;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
 
 public class PhaseDimension implements ModInitializer {
     /**
@@ -8,6 +10,12 @@ public class PhaseDimension implements ModInitializer {
      */
     @Override
     public void onInitialize() {
+        ModItems.initialize();
 
+        ItemGroupEvents
+                // Register a "modify" event for the Ingredients item group.
+                .modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                // Add the item to the group when you get access to it.
+                .register((itemGroup) -> itemGroup.add(ModItems.POOP));
     }
 }
