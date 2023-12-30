0d93d9693e1c75d1e10986d1c9296025ee36d760
ThePhaseCat <alexmgamer10@gmail.com>
2023-12-29 17:33:11 -0700

    first commit

---
+++ src/main/java/phase/phase_dimension/phasedimension/PhaseDimension.java
@@ -0,0 +1,13 @@
+package phase.phase_dimension.phasedimension;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PhaseDimension implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/phase/phase_dimension/phasedimension/client/PhaseDimensionClient.java
@@ -0,0 +1,13 @@
+package phase.phase_dimension.phasedimension.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PhaseDimensionClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
